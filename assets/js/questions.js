2e62592fd6fb5461ac2d60006b836726f1babbe8
jpugmire <jospugg@gmail.com>
2022-05-23 15:23:20 -0400

    add files

---
+++ assets/js/questions.js
@@ -0,0 +1,29 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "What's the best language to learn?",
+    choices: ["CSS", "Javascript", "Typescript", "all of the above"],
+    answer: "Typescript"
+  },
+  {
+    title: "Am I waiting for the react unit impatiently?",
+    choices: ["yes", "no"],
+    answer: "yes"
+  },
+  {
+    title: "This course is _______ weeks long.",
+    choices: [
+      "12",
+      "a million",
+      "uncountable",
+      "all of the above"
+    ],
+    answer: "all of the above"
+  },
+  {
+    title:
+      "I have ______ homework assignments left to do.",
+    choices: ["12", "a million", "uncountable", "all of the above"],
+    answer: "all of the above"
+  }
+];
